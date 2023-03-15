e3c5ea6070c15d2f776ba7a3afb0ea9bfcb6f263
lucas <schneider.lusca@gmail.com>
2023-03-14 21:05:46 -0300

    lista-1

--- exercicio2-lista1/main.cpp
+++ exercicio2-lista1/main.cpp
@@ -129,8 +129,7 @@ int main()
 
 		glUniform4f(colorLoc, 0.0f, 0.0f, 1.0f, 1.0f); //enviando cor para vari�vel uniform inputColor
 
-		// glDrawArrays(GL_TRIANGLES, 0, 3); // triangulo
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 6); //preenche a forma
 
 		// Chamada de desenho - drawcall
 		// CONTORNO - GL_LINE_LOOP
@@ -138,8 +137,9 @@ int main()
 
 		glUniform4f(colorLoc, 1.0f, 0.0f, 1.0f, 1.0f); //enviando cor para vari�vel uniform inputColor
 		
-		//glDrawArrays(GL_LINE_LOOP, 0, 3); //triangulo
-		glDrawArrays(GL_LINE_LOOP, 0, 6);
+		glDrawArrays(GL_LINE_LOOP, 0, 6); //desenha a linha (contorno)
+
+		glDrawArrays(GL_POINTS, 0, 6); //desenha os pontos nos vertices
 
 
 		glBindVertexArray(0); //Desconectando o buffer de geometria
